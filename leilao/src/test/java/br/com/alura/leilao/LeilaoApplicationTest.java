682deb9c3512f29ea708bba3b45822584d8c539a
gabryelboeira <gabryel392@gmail.com>
2022-08-11 16:37:21 -0300

    Existem diversos módulos do Selenium, dentre eles o WebDriver; Aprendemos como adicionar o Selenium WebDriver no projeto, como um dependendência do Maven; Aprendemos a adicionar o Driver do navegador em nosso projeto; Aprendemos a escrever um código de teste que abre uma janela do navegador e navega para uma página, utilizando a API do selenium WebDriver.

---
+++ leilao/src/test/java/br/com/alura/leilao/LeilaoApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaoApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ leilao/src/test/java/br/com/alura/leilao/SeleniumHelloWordTest.java
@@ -0,0 +1,21 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class SeleniumHelloWordTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver_104");
+    }
+
+    @Test
+    void helloWord() {
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("http://localhost:8080/leiloes");
+        browser.quit();
+    }
+}
